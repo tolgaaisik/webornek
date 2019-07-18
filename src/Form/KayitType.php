8590a5d7faa685206c16cca21d30f41358dc781d
tolgaaisik <52401166+tolgaaisik@users.noreply.github.com>
2019-07-18 17:48:56 +0300

    Add files via upload

--- src/Form/KayitType.php
+++ src/Form/KayitType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Security\Core\Authorization;
 
@@ -17,10 +18,10 @@ class KayitType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('KullaniciAdi', TextType::class)
-            ->add('Sifre', TextType::class)  
-            ->add('Email', EmailType::class)
-            ->add('Kayit Ol', SubmitType::class)
+          ->add('KullaniciAdi', TextType::class)
+          ->add('Sifre', PasswordType::class)
+          ->add('Email', EmailType::class)
+          ->add('Gonder', SubmitType::class)
         ;
     }
 
